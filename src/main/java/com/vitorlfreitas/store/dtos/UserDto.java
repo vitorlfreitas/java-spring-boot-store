8516cabe30589816f720f94a7a7292689e976368
vitorlfreitas <vitor.lucfreitas@gmail.com>
2025-09-20 00:35:47 +0100

    Introduces how to customize the Json, by excluding null values, formatting the data, Ignoring data to be sent and others using the @Json% annotations.

--- src/main/java/com/vitorlfreitas/store/dtos/UserDto.java
+++ src/main/java/com/vitorlfreitas/store/dtos/UserDto.java
@@ -1,15 +1,26 @@
 package com.vitorlfreitas.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
 
+    // Besides JsonIgnore, we have more options,
+    // JsonProperty("new name") for renaming a field
+    @JsonIgnore // Removes id from being serialized
     private Long id;
     private String name;
     private String email;
+    @JsonInclude(JsonInclude.Include.NON_NULL) // Exclude null fields
+    private String phoneNumber;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") // Format data
+    private LocalDateTime createdAt;
 
 }
--- src/main/java/com/vitorlfreitas/store/mappers/UserMapper.java
+++ src/main/java/com/vitorlfreitas/store/mappers/UserMapper.java
@@ -3,9 +3,11 @@ package com.vitorlfreitas.store.mappers;
 import com.vitorlfreitas.store.dtos.UserDto;
 import com.vitorlfreitas.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }

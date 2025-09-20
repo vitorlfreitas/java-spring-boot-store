2b81095b17b7ffa87137f86e20cd74b9ccf7a24a
vitorlfreitas <vitor.lucfreitas@gmail.com>
2025-09-20 04:23:22 +0100

    Demonstrate how to implement a custom business rule validation, as an example, we  use a validation to verify if an email is already registered

--- src/main/java/com/vitorlfreitas/store/controllers/UserController.java
+++ src/main/java/com/vitorlfreitas/store/controllers/UserController.java
@@ -11,11 +11,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -66,8 +64,16 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@Valid @RequestBody RegisterUserRequest request,
-                                              UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterUserRequest request,
+                                                UriComponentsBuilder uriBuilder) {
+
+        // Checks if the email is already registered
+        if (userRepository.existsByEmail(request.getEmail())) {
+            return ResponseEntity.badRequest().body(
+                    Map.of("email", "Email is already registered")
+            );
+        };
+
         // Convert the incoming request DTO (data sent by client) into a User entity
         var user = userMapper.toEntity(request);
 

b1ce528de17517f96c527dc44fb9699cbcbb1218
vitorlfreitas <vitor.lucfreitas@gmail.com>
2025-09-28 19:07:34 +0100

    Implements a new endpoint getCart. Also implements best practices and optimizations for when fetching data from database.

--- src/main/java/com/vitorlfreitas/store/controllers/CartController.java
+++ src/main/java/com/vitorlfreitas/store/controllers/CartController.java
@@ -48,7 +48,7 @@ public class CartController {
             @RequestBody AddItemToCartRequest request
             ) {
 
-        var cart = cartRepository.findById(cartId).orElse(null);
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
 
         if (cart == null)
             return ResponseEntity.notFound().build();
@@ -58,7 +58,7 @@ public class CartController {
         if (product == null)
             return ResponseEntity.badRequest().build();
 
-        var cartItem = cart.getCartItems().stream()
+        var cartItem = cart.getItems().stream()
                 .filter(item ->
                         item.getProduct()
                                 .getId()
@@ -74,7 +74,7 @@ public class CartController {
             cartItem.setProduct(product);
             cartItem.setQuantity(1);
             cartItem.setCart(cart);
-            cart.getCartItems().add(cartItem);
+            cart.getItems().add(cartItem);
         }
 
         cartRepository.save(cart);
@@ -82,5 +82,16 @@ public class CartController {
         var cartItemDto = cartMapper.toDto(cartItem);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
+    }
+
+    @GetMapping("/{cartId}")
+    public ResponseEntity<CartDto> getCart(@PathVariable UUID cartId) {
+        var cart = cartRepository.getCartWithItems(cartId)
+                                    .orElse(null);
+
+        if (cart == null)
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(cartMapper.toDto(cart));
     }
 }
--- src/main/java/com/vitorlfreitas/store/mappers/CartMapper.java
+++ src/main/java/com/vitorlfreitas/store/mappers/CartMapper.java
@@ -10,8 +10,11 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface CartMapper {
 
+    @Mapping(target = "totalPrice",
+            expression = "java(cart.getTotalPrice())")
     CartDto toDto(Cart cart);
 
-    @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
+    @Mapping(target = "totalPrice",
+            expression = "java(cartItem.getTotalPrice())")
     CartItemDto toDto(CartItem cartItem);
 }
